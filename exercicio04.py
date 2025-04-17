f16145e0af5ed91d2a362b9f29b7775250b35776
thomthomtt <thomazgt39@gmail.com>
2025-04-17 14:45:30 -0300

    salario minimo

---
+++ exercicio04.py
@@ -0,0 +1,4 @@
+salariomin=float(input("Digite o valor do salario minimo: "))
+salario=float(input("Digite o valor do salario: "))
+qtd=salario/salariomin
+print(f"A quantidade de salarios minimos recebidos é: {qtd:.2f}")
