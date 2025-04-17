850dbd3b23c09f4b418a64b7c04bbe626b198a67
thomthomtt <thomazgt39@gmail.com>
2025-04-17 14:01:47 -0300

    exercicio de revisao 01

--- exercicio01.py
+++ exercicio01.py
@@ -2,9 +2,8 @@ num1=int(input("Digite um numero: "))
 num2=int(input("Digite um numero: "))
 num3=int(input("Digite um numero: "))
 soma=num1+num2
-print(f"A soma entre a e b é: {soma}")
 if soma < num3:
-    print(f"A soma é menor que c")
+    print(f"A soma entre a e b é: {soma} e é menor que c")
 elif soma==num3 :
     print(f"A soma é igual a c")
 else:
