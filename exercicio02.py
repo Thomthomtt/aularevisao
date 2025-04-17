e5da9d71337dfa931d5f6aa253b0dfd7f179ff50
thomthomtt <thomazgt39@gmail.com>
2025-04-17 14:12:20 -0300

    impar ou par, positivo ou negativo

---
+++ exercicio02.py
@@ -0,0 +1,10 @@
+num=float(input("Digite um numero qualquer: "))
+mod=num%2
+if mod==0:
+    print(f"O número é par")
+else:
+    print(f"O número é impar")
+if num<0:
+    print(f"O número é negativo")
+else:
+    print(f"O número é pósitivo")
